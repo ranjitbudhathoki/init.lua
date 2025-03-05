ce47138863ae00544e4264bc622cedea07d3b5dd
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2025-03-05 11:48:02 +0545

    fix: paste from system clipboard with leader<p>

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -18,6 +18,8 @@ vim.keymap.set("n", "<C-u>" ,"<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", "\"_dp")
+
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
 vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
